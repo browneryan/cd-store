451873b97c9aa4b45d876b88e192d6b3bf74b0d3
michael lor and ryan brown <browneryan87@gmail.com>
2016-05-09 10:50:08 -0700

    Initial commit

---
+++ sites/default/files/php/twig/3761cf5c_confirm-form.html.twig_a1ea98b6e5d534a15ded65ae98eb50f4e55e7f72f346d8bd1c0ca008d4ad65f2/dd74d82bbc685c47b11af03e4b1fbe296da9c1173e6d63ddd70ccca8f13ff23a.php
@@ -0,0 +1,76 @@
+<?php
+
+/* core/themes/classy/templates/form/confirm-form.html.twig */
+class __TwigTemplate_f6594cf9a8a22ba900843a6f4f25691bfd1e9d7de7b03c31c61a9b03bf40ed09 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $tags = array();
+        $filters = array();
+        $functions = array();
+
+        try {
+            $this->env->getExtension('sandbox')->checkSecurity(
+                array(),
+                array(),
+                array()
+            );
+        } catch (Twig_Sandbox_SecurityError $e) {
+            $e->setTemplateFile($this->getTemplateName());
+
+            if ($e instanceof Twig_Sandbox_SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+        // line 13
+        echo $this->env->getExtension('sandbox')->ensureToStringAllowed($this->env->getExtension('drupal_core')->escapeFilter($this->env, (isset($context["form"]) ? $context["form"] : null), "html", null, true));
+        echo "
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "core/themes/classy/templates/form/confirm-form.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  43 => 13,);
+    }
+}
+/* {#*/
+/* /***/
+/*  * @file*/
+/*  * Theme override for confirm form.*/
+/*  **/
+/*  * By default this does not alter the appearance of a form at all,*/
+/*  * but is provided as a convenience for themers.*/
+/*  **/
+/*  * Available variables:*/
+/*  * - form: The confirm form.*/
+/*  *//* */
+/* #}*/
+/* {{ form }}*/
+/* */
